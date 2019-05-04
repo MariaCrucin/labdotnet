fe2a5cc814d5b00269e6072699fbd32a64dcbd9d
Maria Crucin <crucin_maria@yahoo.com>
2019-05-04 10:50:18 +0300

    Finalizare Lab1

---
+++ WebApplication1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProductsDbSeeder
+    {
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any flowers.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Televizor",
+                    Description = "Pentru Vizualizare",
+                    Category = "Electronice",
+                    Price = 5.00
+                },
+                new Product
+                {
+                    Name = "Frigider",
+                    Description = "Pentru Racire",
+                    Category = "Frigorifice",
+                    Price = 15.00
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
